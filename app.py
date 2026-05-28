403815604dfa802e10bf00e4e849b673eb0d01d1
eliebelopes85-cmd <eliebelopes85@gmail.com>
2026-05-28 18:07:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -173,9 +173,7 @@ if resposta_jogos.status_code == 200:
 
         nome = f"{casa} x {fora}"
 
-        fixture_id = jogo["fixture"]["id"]
-
-        lista_jogos[nome] = fixture_id")
+        fixture_id = jogo["fixture"]["idlista_jogos[nome] = fixture_id")
    if len(lista_jogos) > 0:
 
     jogo_escolhido = st.selectbox(
