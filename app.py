a3ede6f43ac2e5dfe194a3740efeb996df82102b
eliebelopes85-cmd <eliebelopes85@gmail.com>
2026-05-29 16:30:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import time
 from datetime import datetime
-from streamlit_autorefresh import st_autorefresh
+
 
 # =====================================================
 # CONFIG APP
